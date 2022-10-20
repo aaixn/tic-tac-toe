fc8df9c58e0c99b0292aa8830489d36e8e64fa0f
aaixn <axnguyen246@gmail.com>
2022-10-20 16:04:57 -0700

    completed with some bonuses

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const player1 = document.querySelector('#player-1')                 // grab players
 const player2 = document.querySelector('#player-2')
 const playAgain = document.querySelector('#play-again')
+const submit = document.querySelector('#submit')
+const player1_name = document.querySelector('#player1_name')
+const player2_name = document.querySelector('#player2_name')
+const modal = document.querySelector('#modal')
 
+const ticTacToeBox = document.querySelectorAll('.tic-tac-toe-box')
 const topRow = document.querySelectorAll('.top-row')                // grab win conditions
 const midRow = document.querySelectorAll('.mid-row')
 const botRow = document.querySelectorAll('.bot-row')
@@ -25,10 +30,7 @@ function checkForWins() {                                                       
             alert(`${player} won the game!`)
             playAgain.style.display = 'block'
         }
-        // else if (i === winConditions.length-1 && checkIfWin === false) {
-        //     alert('It\'s a tie')
-        // }
-    }
+    } return false
 }
 
 let taken = false
@@ -37,6 +39,16 @@ function checkIfTaken() {
         taken = true
     } else {
         taken = false
+    }
+}
+
+
+function checkIfAllFilled() {
+    let boxesArray = Array.from(ticTacToeBox)
+    const allFilled = boxesArray.every(element => element.innerText !== '')
+    if (allFilled === true && checkForWins() === false) {
+        alert('Its a tie')
+        playAgain.style.display =  'block'
     }
 }
 
@@ -57,13 +69,13 @@ let turn = 0
 let player = null
 function whoseTurn (turn) {
     if (turn%2 === 1) {
-        player = 'Player 1'
+        player = player1.innerText
         player1.style.color = '#CF616F'
         player1Choice()
         player1.style.color = 'white'
         player2.style.color = '#61CFC1'
     } else if (turn%2 === 0){
-        player = 'Player 2'
+        player = player2.innerText
         player2.style.color = '#61CFC1'
         player2Choice()
         player2.style.color = 'white'
@@ -72,7 +84,6 @@ function whoseTurn (turn) {
 }
 
 
-const ticTacToeBox = document.querySelectorAll('.tic-tac-toe-box')
 ticTacToeBox.forEach(element => {
     element.addEventListener('click', evt => {
         evt.preventDefault()
@@ -81,6 +92,7 @@ ticTacToeBox.forEach(element => {
         if (taken === false) {
             whoseTurn(turn)
             checkForWins()
+            checkIfAllFilled()
         } else if (taken === true ) {
             alert('That spot is taken! Choose another.')
             turn--
@@ -89,3 +101,27 @@ ticTacToeBox.forEach(element => {
 )})
 
 
+playAgain.addEventListener('click', evt => {
+    evt.preventDefault()
+    ticTacToeBox.forEach(element => element.innerText = '')
+    turn = 0
+    player1.style.color = '#CF616F'
+    player2.style.color = 'white'
+})
+
+
+submit.addEventListener('click', evt => {
+    evt.preventDefault()
+    if (player1_name.value === '') {
+        player1.innerText = 'Player 1'
+    } else {
+        player1.innerText = player1_name.value
+    }
+
+    if (player2_name.value === '') {
+        player2.innerText = 'Player 2'
+    } else {
+        player2.innerText = player2_name.value
+    }
+    modal.style.display = 'none'
+})
